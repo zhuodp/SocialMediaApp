3be681bfe4267e5fd04762ab1c514cc3628b727e
zhuodp <zhuodp@fenbi.com>
2024-09-16 16:41:10 +0800

    RFCT: make story list a view component

--- App.tsx
+++ App.tsx
@@ -6,76 +6,16 @@
  * @format
  */
 
-import React, {useState} from 'react';
-import {
-  FlatList,
-  Pressable,
-  SafeAreaView,
-  ScrollView,
-  Text,
-  View,
-} from 'react-native';
-import Title from './components/title/Title';
+import React from 'react';
+import {Pressable, SafeAreaView, ScrollView, Text, View} from 'react-native';
+
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
 import style from './assets/style/mian';
-import UserStory from './components/UserStory/UserStory';
+import UserStoryList from './components/UserStoryList';
+import Title from './components/title/Title';
 
 const App = () => {
-  // all of the items in the stories.
-  const data = [
-    {
-      firstName: 'Jhon',
-      id: 1,
-    },
-    {
-      firstName: 'Amie',
-      id: 2,
-    },
-    {
-      firstName: 'Gavin',
-      id: 3,
-    },
-    {
-      firstName: 'Jhon1',
-      id: 4,
-    },
-    {
-      firstName: 'Amie1',
-      id: 5,
-    },
-    {
-      firstName: 'Gavin1',
-      id: 6,
-    },
-    {
-      firstName: 'Gavin2',
-      id: 7,
-    },
-    {
-      firstName: 'Gavin3',
-      id: 8,
-    },
-    {
-      firstName: 'Gavin4',
-      id: 9,
-    },
-  ];
-
-  const pageSize = 4;
-  const [pageNumber, setPageNumber] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [renderData, setRenderData] = useState(data.slice(0, pageSize));
-
-  const pagination = (data, pageNumber, pageSize) => {
-    let startIndex = (pageNumber - 1) * pageSize;
-    if (startIndex > data.length) {
-      return [];
-    }
-    setPageNumber(pageNumber);
-    return data.slice(startIndex, startIndex + pageSize);
-  };
-
   return (
     <SafeAreaView>
       <ScrollView>
@@ -89,26 +29,7 @@ const App = () => {
           </Pressable>
         </View>
         <View style={style.userStoryContainer}>
-          <FlatList
-            onEndReachedThreshold={0.1}
-            keyExtractor={item => item.id.toString()}
-            onEndReached={() => {
-              if (!isLoading) {
-                setIsLoading(true);
-                setRenderData(prev => [
-                  ...prev,
-                  ...pagination(data, pageNumber + 1, pageSize),
-                ]);
-                setIsLoading(false);
-              }
-            }}
-            showsHorizontalScrollIndicator={false}
-            horizontal={true}
-            data={renderData}
-            renderItem={({item}) => {
-              return <UserStory firstName={item.firstName} />;
-            }}
-          />
+          <UserStoryList />
         </View>
       </ScrollView>
     </SafeAreaView>
---
+++ components/UserStoryList.js
@@ -0,0 +1,86 @@
+import React from 'react';
+
+import {FlatList, View} from 'react-native';
+import UserStory from './UserStory/UserStory';
+import { useState } from 'react';
+
+const UserStoryList = () => {
+  // all of the items in the stories.
+  const data = [
+    {
+      firstName: 'Jhon',
+      id: 1,
+    },
+    {
+      firstName: 'Amie',
+      id: 2,
+    },
+    {
+      firstName: 'Gavin',
+      id: 3,
+    },
+    {
+      firstName: 'Jhon1',
+      id: 4,
+    },
+    {
+      firstName: 'Amie1',
+      id: 5,
+    },
+    {
+      firstName: 'Gavin1',
+      id: 6,
+    },
+    {
+      firstName: 'Gavin2',
+      id: 7,
+    },
+    {
+      firstName: 'Gavin3',
+      id: 8,
+    },
+    {
+      firstName: 'Gavin4',
+      id: 9,
+    },
+  ];
+
+  const pageSize = 4;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [renderData, setRenderData] = useState(data.slice(0, pageSize));
+
+  const pagination = (data, pageNumber, pageSize) => {
+    let startIndex = (pageNumber - 1) * pageSize;
+    if (startIndex > data.length) {
+      return [];
+    }
+    setPageNumber(pageNumber);
+    return data.slice(startIndex, startIndex + pageSize);
+  };
+
+  return (<View>
+    <FlatList
+      onEndReachedThreshold={0.1}
+      keyExtractor={item => item.id.toString()}
+      onEndReached={() => {
+        if (!isLoading) {
+          setIsLoading(true);
+          setRenderData(prev => [
+            ...prev,
+            ...pagination(data, pageNumber + 1, pageSize),
+          ]);
+          setIsLoading(false);
+        }
+      }}
+      showsHorizontalScrollIndicator={false}
+      horizontal={true}
+      data={renderData}
+      renderItem={({item}) => {
+        return <UserStory firstName={item.firstName} />;
+      }}
+    /></View>
+  );
+};
+
+export default UserStoryList;
